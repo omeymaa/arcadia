266f94be4405c9d4be23b942c236d7d52ff9d973
Omeyma <omeyma.maatoug@gmail.com>
2024-09-26 23:31:41 +0200

    adjustement homeController and home css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
@@ -15,29 +16,27 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        // Récupérer les avis acceptés
-        $comments = $entityManager->getRepository(Comment::class)->findBy(['status' => 'accepte']);
+        $commentaire = new Comment();
+        $form = $this->createForm(CommentType::class, $commentaire);
 
-        // Création du formulaire pour un nouvel avis
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class, $comment);
-        $form->handleRequest($request);
-        
-        // Traitement du formulaire via AJAX
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($comment);
-            $entityManager->flush();
-        
-            // Si la requête est AJAX
-            if ($request->isXmlHttpRequest()) {
-                return $this->json(['message' => 'Votre avis a été soumis pour validation.']);
+        // Traite la soumission AJAX
+        if ($request->isXmlHttpRequest()) {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $commentaire->setStatus('en attente'); // Définit le statut à "en attente"
+                $entityManager->persist($commentaire);
+                $entityManager->flush();
+
+                return new JsonResponse(['message' => 'Votre avis a été soumis pour validation.']);
             }
-        }
+    }
+        
         
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'form' => $form->createView(),
-            'commentaires' => $comments,
+            'comments' => $entityManager->getRepository(Comment::class)->findBy(['status' => 'accepte']),
         ]);
     }
 }
