ac5f0f6fdab804c94da6624feeac4197e095239b
Omeyma <omeyma.maatoug@gmail.com>
2024-09-27 18:33:20 +0200

    add created_at in comment entity

---
+++ migrations/Version20240927163235.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240927163235 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment ADD created_at DATETIME NOT NULL, CHANGE pseudonym pseudonym VARCHAR(50) NOT NULL, CHANGE comment comment TINYTEXT NOT NULL, CHANGE status status VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP created_at, CHANGE pseudonym pseudonym VARCHAR(255) NOT NULL, CHANGE comment comment LONGTEXT NOT NULL, CHANGE status status VARCHAR(255) NOT NULL');
+    }
+}
